11fe3a4e45d23d46c7a58cdf2d771f6d57cb702e
Jiradet Amornpimonkul <jiradet.amorn@gmail.com>
2025-10-28 00:01:19 -0400

    Initial Commit

---
+++ Reskills/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reskills
+//
+//  Created by Jiradet Amornpimonkul on 10/28/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
