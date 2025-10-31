b810a6cfa9834b34cfd70b75ea67945add292bce
Jiradet Amornpimonkul <jiradet.amorn@gmail.com>
2025-10-30 21:39:05 -0400

    Task List
    add task model
    fix task cell

---
+++ Reskills/Models/TaskModel.swift
@@ -0,0 +1,14 @@
+//
+//  TaskModel.swift
+//  Reskills
+//
+//  Created by Jiradet Amornpimonkul on 10/30/25.
+//
+import UIKit
+
+struct TaskModel {
+//    let id: UUID
+    var title: String
+    var notes: String?
+    var isCompleted: Bool
+}
--- Reskills/ViewControllers/TaskListViewController.swift
+++ Reskills/ViewControllers/TaskListViewController.swift
@@ -11,7 +11,7 @@ class TaskListViewController: UIViewController {
     
     private let testLabel = UILabel()
     private let tasksTableView = UITableView()
-    private var tasks: [String] = []
+    private var tasks: [TaskModel] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +26,8 @@ class TaskListViewController: UIViewController {
         tasksTableView.delegate = self
         tasksTableView.dataSource = self
         tasksTableView.translatesAutoresizingMaskIntoConstraints = false
+        tasksTableView.estimatedRowHeight = 72
+        tasksTableView.rowHeight = UITableView.automaticDimension
         tasksTableView.register(TaskCell.self, forCellReuseIdentifier: TaskCell.reuseID)
         tasksTableView.separatorStyle = .none
         view.addSubview(tasksTableView)
@@ -37,6 +39,18 @@ class TaskListViewController: UIViewController {
             tasksTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
         
+        tasks = [
+            TaskModel(title: "Buy groceries and some other very long title to test wrapping",
+                      notes: "Milk, eggs, bread, and fruits",
+                      isCompleted: false),
+            TaskModel(title: "Read a book",
+                      notes: "At least 20 pages today",
+                      isCompleted: true),
+            TaskModel(title: "Workout",
+                      notes: nil,
+                      isCompleted: false)
+        ]
+        tasksTableView.reloadData()
     }
     
     @objc private func addTask() {
@@ -51,19 +65,31 @@ class TaskListViewController: UIViewController {
 extension TaskListViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        10
+        tasks.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath) as? TaskCell {
-            
-            return cell
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TaskCell.reuseID, for: indexPath) as? TaskCell else {
+            return UITableViewCell()
         }
-        return UITableViewCell()
+        let item = tasks[indexPath.row]
+        cell.configure(title: item.title, subtitle: item.notes, isCompleted: item.isCompleted)
+        cell.accessoryType = .disclosureIndicator
+        cell.selectionStyle = .none
+        return cell
     }
 }
 
 extension TaskListViewController: UITableViewDelegate {
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        // Toggle completion state
+        self.tasks[indexPath.row].isCompleted.toggle()
+        // Haptic feedback
+        UINotificationFeedbackGenerator().notificationOccurred(.success)
+        // Reload only this row to reflect the new state
+        tableView.reloadRows(at: [indexPath], with: .none)
+    }
 }
 
